d267f3d141eb081d156cc4849a250b716417c036
Shruti Bhale <shrutibhale2@gmail.com>
2026-03-16 22:52:33 +0530

    Update PredictPage.tsx

--- frontend/src/pages/PredictPage.tsx
+++ frontend/src/pages/PredictPage.tsx
@@ -47,7 +47,7 @@ const PredictPage = () => {
   const handlePredict = async () => {
     try {
       // 1. Send the form data to our FastAPI backend
-      const response = await fetch("https://customer-churn-prediction-3qtx.onrender.com/api/predict/single", {
+      const response = await fetch("https://customer-churn-prediction-3qtx.onrender.com/api/predict/single/api/predict/single", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
